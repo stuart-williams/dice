5698c7f9961b15fb828d43557731b1dbc4a5e5b2
Stuart Williams <stuart@tickx.co.uk>
2022-07-15 14:06:27 +0100

    feat: format 0 value ticket prices

--- hooks/useFormatTicketPrice.ts
+++ hooks/useFormatTicketPrice.ts
@@ -4,13 +4,20 @@ import type * as Api from "types/api";
 export const useFormatTicketPrice = () => {
   const { locale } = useRouter();
 
-  return (price: Api.TicketPrice, currency = "GBP") =>
+  return (price: Api.TicketPrice, currency = "GBP") => {
     /**
      * at TickX we are required by our US clients (and I guess law) to show prices
      * as early as possible inc. fees, taxes etc. hence the ticket price object.
      */
-    new Intl.NumberFormat(locale, {
+
+    if (!price.face_value) {
+      return "Free";
+    }
+
+    return new Intl.NumberFormat(locale, {
       currency,
       style: "currency",
+      currencyDisplay: "narrowSymbol",
     }).format(price.face_value / 100);
+  };
 };
